37c1ea4a3d4e612a83a4a1f0a4f011750f8bf742
Palmisano-enzo <enzo.palmisano@epfl.ch>
2023-06-30 22:31:59 +0200

    use odoo native method

--- custom/more-pdf/models/ir_action_report_more_pdf.py
+++ custom/more-pdf/models/ir_action_report_more_pdf.py
@@ -1,58 +1,34 @@
 from odoo import models, _
 from odoo.exceptions import UserError
-from PyPDF2 import PdfFileReader, PdfFileMerger
-import img2pdf
-from PIL import Image
-from io import BytesIO
 
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
+    #override _render_qweb_pdf to merge pdf and image attachment in one PDFs
     def _render_qweb_pdf(self, docids=None, data=None):
         if self.report_name == 'account.report_original_vendor_bill':
             
             docs = self.env['account.move'].browse(docids)
 
-            #table of PDFFiles object to merge
-            pdf_to_merge = []
+            #table of stream to merge
+            stream_to_merge = []
 
             for doc in docs:
                 # Get attachments for each doc
                 attachments = self.env['ir.attachment'].search([('res_model', '=', self.model), ('res_id', '=', doc.id)])
+                
                 # Process attachments
                 for attachment in attachments:
-                    # attachement in PDF format
-                    if attachment.mimetype == 'application/pdf':
-                        attachment_pdf = PdfFileReader(attachment._full_path(attachment.store_fname))
-                        pdf_to_merge.append(attachment_pdf)
-                    # attachement in image format
-                    if attachment.mimetype.startswith('image'):
-                        # convert image to PDF
-                        attachment_img = attachment._full_path(attachment.store_fname)
-                        attachment_pdf = self._image_to_pdf(attachment_img)
-                        pdf_to_merge.append(attachment_pdf)
-                    #add PDFFile object to table of PDFs to merge
-                    
+                    # attachement in PDF or image format
+                    if attachment.mimetype == 'application/pdf' or attachment.mimetype.startswith('image'):
+                        attachment_pdf = self._retrieve_stream_from_attachment(attachment)
+                        stream_to_merge.append(attachment_pdf)
             
             #no attachment found
-            if len(pdf_to_merge) == 0:
+            if len(stream_to_merge) == 0:
                 raise UserError(_("No original vendor bills could be found for any of the selected vendor bills."))
             
-            #merge PDFs
-            merged_pdf = PdfFileMerger(strict=True)
-            for pdf in pdf_to_merge:
-                merged_pdf.append(pdf, import_bookmarks=False) 
-                #import_bookmarks=False to avoid error Unexpected destination '/__WKANCHOR_2'
-
-            #render merged PDF to PDF content
-            data_stream = BytesIO()
-            merged_pdf.write(data_stream)
-            data_stream.seek(0)
-            return data_stream.getvalue(), 'pdf'
+            #merge stream to PDFs
+            return self._merge_pdfs(stream_to_merge), 'pdf'
 
         return super(IrActionsReport, self)._render_qweb_pdf(docids, data=data)
-
-    def _image_to_pdf(self, attachment_path):
-        image = Image.open(attachment_path)
-        pdf_bytes = img2pdf.convert(image.filename)
-        return PdfFileReader(BytesIO(pdf_bytes))
